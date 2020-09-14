7d183ef785da65ab0e96c6bacd3ae9af1d916f4c
ALI REMAITY <aliremaitybook@gmail.com>
2020-09-14 10:55:57 +0300

    init WH_CRUD class

--- webhook/receive.php
+++ webhook/receive.php
@@ -1,5 +1,12 @@
 <?php
 
+
+define('SHOPIFY_APP_SECRET', "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");
+$hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+$data = file_get_contents('php://input');
+$verified = verify_webhook($data, $hmac_header);
+error_log('Webhook verified: '.var_export($verified, true) ,3, "result.txt");
+
 // Load variables
 $webhook_content = NULL;
 
@@ -15,4 +22,11 @@ $webhook_content = json_decode($webhook_content, TRUE);
 $fp = fopen('result.json', 'w');
 fwrite($fp, json_encode($webhook_content));
 fclose($fp);
+
+function verify_webhook($data, $hmac_header)
+{
+  $calculated_hmac = base64_encode(hash_hmac('sha256', $data, SHOPIFY_APP_SECRET, true));
+  return hash_equals($hmac_header, $calculated_hmac);
+}
+
 ?>

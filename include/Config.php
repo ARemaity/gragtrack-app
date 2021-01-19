febb25d7a0365bb2166dad7a5299f4a3ab1dca96
ALI REMAITY <aliremaitybook@gmail.com>
2021-01-19 09:05:21 +0200

    Update Config.php

--- include/Config.php
+++ include/Config.php
@@ -1,9 +1,6 @@
 <?php
 
-// define("DB_HOST", "localhost");
-// define("DB_USER", "gragtrack_it");
-// define("DB_PASSWORD", "wzgGkzsKxgV9rWL");
-// define("DB_DATABASE", "gragtrack_app");
+
 
 
 define("DB_HOST", "localhost");
